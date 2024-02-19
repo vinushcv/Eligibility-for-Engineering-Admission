6ba0c707a4233f7e58a19476ef5d951398d9e4b4
vinushcv <yaya.usha@gmail.com>
2024-02-19 11:52:09 +0530

    first

--- Program.cs
+++ Program.cs
@@ -3,7 +3,26 @@
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hiiiiiii");
+           int m1,m2,m3;
+           Console.Write("Enter Maths marks:");
+           m1=Convert.ToInt32(Console.ReadLine());
+           Console.Write("Enter Physics marks:");
+           m2=Convert.ToInt32(Console.ReadLine());
+           Console.Write("Enter Chemistry marks:");
+           m3=Convert.ToInt32(Console.ReadLine());
+           int total=m1+m2+m3;
+           int mathphy=m1+m2;
+           if(m1>=65 && m2>=55 && m3>=50)
+           {
+            if(total>=180 || mathphy>=140)
+            {
+                Console.WriteLine("The candidate is eligible for the admission.");
+            }
+            
+           }
+           else
+           {
+            Console.WriteLine("The candidate is not eligible for the admission.");
+           }
         }
     }
-
--- obj/Debug/net6.0/Eligibility-for-Engineering-Admission.AssemblyInfo.cs
+++ obj/Debug/net6.0/Eligibility-for-Engineering-Admission.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8448d33462d08235f67174abd65663c65ca692a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
